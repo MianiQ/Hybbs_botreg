5e447adfef44ce45ae41801a95503b489eae3fc7
MianiQ <108111363+MianiQ@users.noreply.github.com>
2022-06-24 12:42:33 +0800

    Update botreg.php

--- botreg.php
+++ botreg.php
@@ -1,14 +1,12 @@
 <?php
 	header("Content-Type: text/html; charset=utf-8");
-	$pass = rand(99999,9999999);
-	$user = rand(99999,99999999);
-	$email = rand(99999,99999999);
+	$pass = rand(99999999,99999999999).rand(99999999,99999999999);
+	$user = rand(99999999,99999999999).rand(1,9999999);
+	$email = rand(9999999,99999999999);
 	
+	$title = '我是脚本 >'.$user.'<';
+	$content = "我给我自己点赞了哦我给我自己点赞了哦我给我自己点赞了哦我给我自己点赞了哦我给我自己点赞了哦我给我自己点赞了哦我给我自己点赞了哦我给我自己点赞了哦我给我自己点赞了哦我给我自己点赞了哦我给我自己点赞了哦我给我自己点赞了哦";
 	
-	$title = '想要同款刷帖脚本吗，点进来看哦 >'.$user.'<';
-	$content = "我是？$user$user$user$user$user$user$user$user$user$user$user$user$user$user$user$user$user$user$user";
-	
-	header('Content-Type: text/html; charset=utf-8');
 	$cookie_file = dirname(__FILE__).'/cookie.txt';
 	//$cookie_file = tempnam("tmp","cookie");
 	//先获取cookies并保存
@@ -45,9 +43,52 @@
 	curl_setopt($ch, CURLOPT_POSTFIELDS, $post_string);
 	$response = curl_exec($ch);
 	curl_close($ch);
+	
 	$response = json_decode($response,true);
 	if (!$response['error']){
 		echo '(Error):'.$response['info'];
 	}	else	{
 		echo '(OK) type: '.$post_string['forum']."\r\n";
+	}
+	$t_id = $response['id'];
+	$post_string = array(
+	'type' => 'thread1',
+	'id' => $t_id
+	);
+	//自动点赞
+	$url = "http://www.outiao.cn/?s=post/vote"; //HYBBS点赞提交URL，F12+发帖可以获得，不知道可以直接只改前面的URL
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie_file); //使?上?获取的cookies
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post_string);
+	$response = curl_exec($ch);
+	curl_close($ch);
+	$response = json_decode($response,true);
+	if (!$response['error']){
+		echo '(Error):'.$response['info']."\r\n";
+	}	else	{
+		echo '(OK) id: '.$post_string['id']."\r\n";
+	}
+	
+	//自动回复
+	$post_string = array(
+	'content' => "我还能自动回复呢",
+	'id' => $t_id,
+	'tmp_md5' => '114514'
+	);
+	
+	$url = "http://www.outiao.cn/?post/post.html"; //HYBBS回复提交URL，F12+发帖可以获得，不知道可以直接只改前面的URL
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie_file); //使?上?获取的cookies
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post_string);
+	$response = curl_exec($ch);
+	curl_close($ch);
+	$response = json_decode($response,true);
+	if (!$response['error']){
+		echo '(Error):'.$response['info']."\r\n";
+	}	else	{
+		echo '(OK) id: '.$post_string['id']."content: ".$post_string['content']."\r\n";
 	}
