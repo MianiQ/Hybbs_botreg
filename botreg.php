ee85be218c8deba662bf50c88b85b0026e73be05
MianiQ <108111363+MianiQ@users.noreply.github.com>
2022-06-24 12:18:21 +0800

    Update botreg.php

--- botreg.php
+++ botreg.php
@@ -6,14 +6,14 @@
 	
 	
 	$title = '想要同款刷帖脚本吗，点进来看哦 >'.$user.'<';
-	$content = '这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容这是内容';
+	$content = "我是？$user$user$user$user$user$user$user$user$user$user$user$user$user$user$user$user$user$user$user";
 	
 	header('Content-Type: text/html; charset=utf-8');
 	$cookie_file = dirname(__FILE__).'/cookie.txt';
 	//$cookie_file = tempnam("tmp","cookie");
 	//先获取cookies并保存
 	
-	$url = "http://114514.1919810/?user/add.html"; //HYBBS注册URL，F12+注册获得提交地址，不知道可以只改前面的的URL
+	$url = "http://www.outiao.cn/?user/add.html"; //HYBBS注册URL，F12+注册获得提交地址，不知道可以只改前面的的URL
 	
 	//$data = "user=$user&email=$user%40114145as.cn&pass1=$pass&pass2=$pass";
 	$post_string = array('user'=>$user,'email'=>$email.'@qq.com','pass1'=>$pass,'pass2'=>$pass);
@@ -33,11 +33,11 @@
 	$post_string = array(
 	'title' => $title,
 	'content' => $content,
-	'forum'=> rand(1,9),
+	'forum'=> rand(8,8),//帖子分类 自己填 F12看提交的
 	'tmp_md5' => 'GGeie',
 	);
 	
-	$url = "http://114514.1919810/?post.html"; //HYBBS发帖提交URL，F12+发帖可以获得，不知道可以直接只改前面的URL
+	$url = "http://www.outiao.cn/?post.html"; //HYBBS发帖提交URL，F12+发帖可以获得，不知道可以直接只改前面的URL
 	$ch = curl_init($url);
 	curl_setopt($ch, CURLOPT_HEADER, 0);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -46,3 +46,4 @@
 	$response = curl_exec($ch);
 	curl_close($ch);
 	echo $response;
+	echo '分类:'.$post_string['forum'];
